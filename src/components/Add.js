e13cf58224b900701767ef008d72ecdcf9f1e652
bera5186 <rbasu611@gmail.com>
2020-03-25 14:26:08 +0530

    Updated the UI with nuemorphism effect and chnged color

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Add.css'
 
 
 function Add({ todos, addTodo }) {
@@ -25,11 +26,11 @@ function Add({ todos, addTodo }) {
                 <div className="row">
                     <div className="col-md-9">
                         <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Enter a New To-Do" id="text" onChange={ handleChange }></input>
+                            <input type="text" className="form-control form-input" placeholder="Enter a New To-Do" id="text" onChange={ handleChange }></input>
                         </div>
                     </div>
                     <div className="col-md-3">
-                        <button type="submit" className="btn btn-success btn-block">Add To-Do</button>
+                        <button type="submit" className="btn btn-success btn-block form-button">Add To-Do</button>
                     </div>
                 </div>
             </form>
