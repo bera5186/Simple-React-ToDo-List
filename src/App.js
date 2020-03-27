27cb638508d7f31c74950dbc11762454d8cfafc2
bera5186 <rbasu611@gmail.com>
2020-03-27 21:07:06 +0530

    Configured App to use local storage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
+
+// components
 import Add from './components/Add'
 import List from './components/List'
-
+import Blank from './components/blank'
 
 class App extends Component {
 
   state = {
-    todos: [
-      
-    ]
+    todos: JSON.parse(localStorage.getItem("todos"))
   }
 
   addTodo = (todo) => {
-    //console.log(todo)
+    
     let todos = [...this.state.todos, todo]
+
+    // add to local storage
+    localStorage.setItem("todos", JSON.stringify(todos))
+
     this.setState({
       todos: todos
-    })
-
+    })    
   }
 
   deleteTodo = (id) => {
     let todos = this.state.todos.filter((todo) => {
       return todo.id !== id
     })
+    // Update todos in local storage
+    localStorage.setItem("todos", JSON.stringify(todos))
     this.setState({
       todos: todos
     })
@@ -34,11 +39,18 @@ class App extends Component {
   }
 
   render() {
+
+    let compToRender ;
+    if(this.state.todos.length > 0) compToRender = <List todos={ this.state.todos } deleteTodo={ this.deleteTodo } updateDoneState={ this.updateDoneState} />
+    else compToRender = <Blank />
     return (
+     
       <div className="container App">
-        <h1 className="text-center mt-4">Simple To-Do List</h1>
+        <h1 className="text-center mt-4">To Dos</h1>
         <Add todos={ this.state.todos } addTodo={ this.addTodo }/>
-        <List  todos={ this.state.todos } deleteTodo={ this.deleteTodo } updateDoneState={ this.updateDoneState }/>
+        
+        { compToRender }
+
       </div>
     );
   }
